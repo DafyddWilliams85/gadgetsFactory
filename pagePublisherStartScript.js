44ac830cdf039930dad21a9c425d3be80ae3a3eb
Dafydd Williams <dafydd@revotio.com>
2019-07-05 15:30:55 +0200

    Added pagePublisherStartScript.js to facilitate the clone of the pagePublisher file

---
+++ pagePublisherStartScript.js
@@ -0,0 +1,30 @@
+const fs = require('fs-extra');
+const jsonfn = require('json-fn');
+const replace_in_file = require('replace-in-file');
+
+
+// const basedata = JSON.parse(fs.readFileSync(__dirname + "/basedata.json"));
+
+var API_URL = process.env.API_URL
+
+var configValue = fs.readFileSync(__dirname +"/js/value/configValue.js").toString('utf8')
+
+console.log(configValue)
+
+// var configValue = fs.readFileSync(__dirname +"/js/value/configValue.js")
+// console.log(configValue)
+
+const results = replace_in_file.sync({
+  files: __dirname +"/js/value/configValue.js",
+  from: /baseUrlPlaceholder/g,
+  to:  API_URL,
+  countMatches: true
+});
+
+
+console.log(results)
+
+
+
+// var configValue2 = jsonfn.parse(fs.readFileSync(__dirname +"/js/value/configValue.js"))
+// console.log(configValue2)
