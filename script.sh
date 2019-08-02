ed3468e04f8d287eada510425b69e2c863aeda7e
DafyddWilliams85 <dafydd@revotio.com>
2019-08-02 12:15:57 +0000

    Added script.sh animations and fixed file upload in transaction forms && validation by removing unnesseserry subforms

--- script.sh
+++ script.sh
@@ -14,27 +14,30 @@ read API_URL
 echo 6/6 Finally the pagePublisher Branch ?
 read pagePublisherBranch
 
-echo  1 = TYPE = $TYPE, 2 = APP_ID = $APP_ID ,3 = GIT_USERNAME = $GIT_USERNAME ,4 = GIT_PASSWORD = $GIT_PASSWORD , 5 = API_URL = $API_URL , 6 = pagePublisherBranch = $pagePublisherBranch ,
+sudo apt-get install fortune cowsay -y
+sudo apt-get install figlet -y
+
+echo  1 = TYPE = $TYPE, 2 = APP_ID = $APP_ID ,3 = GIT_USERNAME = $GIT_USERNAME ,4 = GIT_PASSWORD = $GIT_PASSWORD , 5 = API_URL = $API_URL , 6 = pagePublisherBranch = $pagePublisherBranch | cowsay ,
 
 now=$(date +"%T")
-echo "Started @ : $now"
+figlet "Started @ : $now"
 
 rm -rf "../../../../var/www/pagePublisher"
 
 # mkdir "../../../../var/www/pagePublisher"
 
-echo "pagePublisher reset completed"
+echo "pagePublisher reset completed" | cowsay
 
 npm install node-cmd && echo node-cmd installed && npm i fs-extra && echo fs-extra installed &&  BRANCH=$pagePublisherBranch GIT_USERNAME=$GIT_USERNAME GIT_PASSWORD=$GIT_PASSWORD node startScript.js
 
-echo  pagePublisher branch : $pagePublisherBranch cloned
+echo  pagePublisher branch : $pagePublisherBranch cloned | cowsay
 
-cd  ../../../../var/www/pagePublisher && echo In PagePublisherFolder && npm i && echo In PagePublisherFolder INSTALLED && npm i fs-extra && echo fs-extra installed && npm i replace-in-file && echo replace-in-file installed && API_URL=$API_URL node pagePublisherStartScript.js
+cd  ../../../../var/www/pagePublisher && figlet In PagePublisherFolder && npm i && figlet In PagePublisherFolder INSTALLED && npm i fs-extra && figlet fs-extra installed && npm i replace-in-file && echo replace-in-file installed && API_URL=$API_URL node pagePublisherStartScript.js
 
-echo  baseUrl in configValue.js file changed to : $API_URL
+echo  baseUrl in configValue.js file changed to : $API_URL | cowsay
  # npm i shelljs &&  echo shelljs installed in pmt_baseApp &&   rsync(settings.js ../../settings.js) && echo >>>>>rsync move completed<<<<< &&
 
-cd ../../../root/.node-red/projects/pmt_baseApp/ && npm i fs-extra && echo fs-extra installed in pmt_baseApp && npm install mime && echo NPM MIME installed in pmt_baseApp && TYPE=$TYPE APP_ID=$APP_ID node flows_script.js
+cd ../../../root/.node-red/projects/pmt_baseApp/ && npm i fs-extra && figlet fs-extra installed in pmt_baseApp && npm install mime && figlet NPM MIME installed in pmt_baseApp && TYPE=$TYPE APP_ID=$APP_ID node flows_script.js
 
 rm -rf "../../../../../root/.node-red/projects/pmt_baseApp/node_modules"
 
@@ -48,5 +51,7 @@ pm2 stop 'pagePublisher = '$TYPE && pm2 del 'pagePublisher = '$TYPE
 pm2 stop $TYPE'- pP' && pm2 del $TYPE'- pP'
 cd ../../../../../../var/www/pagePublisher && TYPE=$TYPE APP_ID=$APP_ID pm2 start 'npm start' --name $TYPE'- pP' -i max --restart-delay=3000 -l ../../../../../../root/pagePublisher.log
 
+figlet DONE :-D
+
 now=$(date +"%T")
-echo "Completed @ : $now"
+echo "Completed @ : $now" | cowsay -f ghostbusters
