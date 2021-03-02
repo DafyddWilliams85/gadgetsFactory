e3387ddc50e0455d2606121826de2da6e0622f27
Dafydd Williams <dafydd@revotio.com>
2021-03-02 15:38:39 +0100

    implemented fix source data removal

--- tmpFile.sh
+++ tmpFile.sh
@@ -2,7 +2,7 @@
 
 # turn on bash's job control
 # turn on bash's job control
-# echo Are you sure you want to clone the new innonationBuilder repo? y/n
+# echo Are you sure you want to clone the new gadgetsFactory repo? y/n
 # read DATA
 DATA=$2
 echo $DATA
@@ -12,13 +12,13 @@ if [ "$DATA" = "y" ]
 then
   pm2 stop all
   figlet pm2 stoped
-  rm -rf ../../../../root/.node-red/projects/innonationBuilder
-  figlet innonationBuilder REVOVED // innonationBuilder script
+  rm -rf ../../../../root/.node-red/projects/gadgetsFactory
+  figlet gadgetsFactory REVOVED // gadgetsFactory script
   npm install node-cmd && echo node-cmd installed && npm i fs-extra && echo fs-extra installed &&  node tmpFile.js
-  figlet innonationBuilder cloned // innonationBuilder script
-  cd ../../../../root/.node-red/projects/innonationBuilder
+  figlet gadgetsFactory cloned // gadgetsFactory script
+  cd ../../../../root/.node-red/projects/gadgetsFactory
   chmod +x script.sh
   source script.sh
 else
-  figlet innonationBuilder NOT cloned // innonationBuilder script
+  figlet gadgetsFactory NOT cloned // gadgetsFactory script
 fi
